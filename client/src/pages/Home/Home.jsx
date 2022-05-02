e4f67c59cd246e705c9de2d0530a859d25fe6a0f
StianOek <66445281+StianOek@users.noreply.github.com>
2022-05-02 22:36:20 +0200

    providing logins/logouts messages and authorization

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,10 +1,27 @@
-import React from "react";
-
+import React, { useContext } from "react";
+import { ProfileContext } from "../../App";
 const Home = () => {
+  const { Gdata } = useContext(ProfileContext);
   return (
     <div>
-      <h1>Mock exam</h1>
-      <p>This is a mock</p>
+      <h1>Web Api Exam 2022</h1>
+      {Gdata.userinfo ? (
+        <div>
+          <h2>Welcome to google {Gdata.userinfo.name}</h2>
+          <p>
+            You can now add movies to the list and you can visit your profile{" "}
+            <a href="/g_profile">here</a>
+          </p>
+        </div>
+      ) : (
+        <div>
+          <h2>
+            Welcome you are.. Please do sign in to either one of the oauth
+            providers. It's located easly up in the top right corner, you see..
+            Cheers lad!
+          </h2>
+        </div>
+      )}
     </div>
   );
 };
